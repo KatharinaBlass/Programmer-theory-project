0383955de45d042a0dedc39d10dd5b025613220a
Kathi <katharina.blass@student.hpi.de>
2021-12-23 20:04:53 +0100

    add Animal parent class and derived classes

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -2,22 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Animal : MonoBehaviour
+//need to be serializable for the save system, so maybe added the attribute just when doing the save system
+[System.Serializable]
+public abstract class Animal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] private float jumpForce = 10.0f;
+    Rigidbody AnimalRb;
 
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        Debug.Log("animal");
+        AnimalRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Key pressed");
+            Jump();
+        }
 
     }
 
     public virtual void Jump()
     {
         Debug.Log("Jump in Animal class");
+        AnimalRb.AddForce(Vector3.up * jumpForce);
     }
+
+
 }
